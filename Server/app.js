8a00ad0489e457d1adc419ac6d839a28e0898dad
EvanDude47 <emthivierge@labs.neit.edu>
2018-02-28 13:38:12 -0500

    Simple multiplayer

--- Server/app.js
+++ Server/app.js
@@ -7,28 +7,30 @@ console.log("Server Started");
 var players = [];
 
 io.on('connection', function(socket){
-    var thisPlayerId = shortid.generate();
-    
-    players.push(thisPlayerId);
-
-    console.log("Client Connected spawning player id: " + thisPlayerId);
-    
-    socket.broadcast.emit('spawn player', {id:thisPlayerId});
-
-    players.forEach(function(playerId){
-        if(playerId === thisPlayerId) return;
-
-        socket.emit('spawn player', {id:playerId});
-        console.log("Adding a new player" + playerId);
-    });
-
-    socket.on('playerhere', function(data){
-        console.log("Player is logged in");
-    });
-
-    socket.on('disconnect', function(){
-        console.log("Player Disconnected");
-        players.splice(players.indexOf(thisPlayerId), 1);
-        socket.broadcast.emit('disconnected', {id:thisPlayerId});
-    });
+	var thisPlayerId = shortid.generate();
+	
+	players.push(thisPlayerId);
+	
+	console.log('client connected spawning player id: '+ thisPlayerId);
+	
+	socket.broadcast.emit('spawn player', {id:thisPlayerId});
+	
+	players.forEach(function(playerId){
+		if(playerId == thisPlayerId)return;
+		
+		socket.emit('spawn player', {id:playerId});
+		console.log("Adding a new player", playerId);
+	});
+	
+	socket.on('move', function(data){
+		data.id = thisPlayerId;
+		console.log("Player position is: ", JSON.stringify(data));
+		socket.broadcast.emit('move', data);
+	});
+	
+	socket.on('disconnect', function(){
+		console.log("Player Disconnected");
+		players.splice(players.indexOf(thisPlayerId),1);
+		socket.broadcast.emit('disconnected', {id:thisPlayerId});
+	});
 });
---
+++ SocketIOProject/SocketIOProject/Assets/NetworkMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+
+public class NetworkMovement : MonoBehaviour {
+	Vector3 pos;
+	public SocketIOComponent socket;
+	
+	// Update is called once per frame
+	void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        pos = transform.position;
+
+		OnMove(pos, h, v);
+	}
+
+	public void OnMove(Vector3 position, float h, float v)
+    {
+		Debug.Log("Sending position to server: " + MovementToJson(position, h, v));
+		socket.Emit("move", new JSONObject(MovementToJson(position, h, v)));
+	}
+
+	string MovementToJson(Vector3 vector, float h, float v)
+    {
+		return string.Format(@"{{""x"":""{0}"", ""y"":""{1}"", ""h"":""{2}"", ""v"":""{3}""}}", vector.x, vector.z, h, v);
+	}
+}
